bc68018ea1848c4d543ab60b190344af84f47c29
MechelleGG <752429@student.dusd.net>
2023-04-20 09:56:27 -0700

    BUttons
    I added the button for restart, i added more to the scripts, and i made the  game objects have particles. I made the game over sign for when the good objects touch the bottom.

--- Unit5UserInterfaceGaldamez/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceGaldamez/Assets/Scripts/GameManager.cs
@@ -9,13 +9,18 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     public TextMeshProUGUI scoreText;
     private int score;
-   
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -24,20 +29,25 @@ public class GameManager : MonoBehaviour
         
     }
 
-    IEnumerator SpawnTarget()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]);
-
-            UpdateScore(5);
-        }
-    }
-    private void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
     }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    
 }
--- Unit5UserInterfaceGaldamez/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceGaldamez/Assets/Scripts/Target.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,8 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -43,11 +49,20 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 }
 
